a3b87eb4ba7566c323e5113129a8e63b7d10d1c0
Sidhra <sidhra.cader@hotmail.com>
2017-10-04 09:28:56 +1100

    completed some server page requests

--- booknook/main.rb
+++ booknook/main.rb
@@ -1,11 +1,20 @@
-     
 require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+require 'pry'
 
 get '/' do
   erb :index
 end
 
+get '/browse-books' do
+  erb :booksearch
+end
 
-
-
-
+get '/books' do
+  search = params[:search]
+  results = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=#{search}&key=AIzaSyBUudw0zTl6mN68yErdEUXrqu8-su1DIWk")
+  data = results.parsed_response
+  @data_array = data["items"]
+  erb :books
+end
